9494509df7e2e41422c5215bfd52f87f61ba9aca
Mahmoud Salman <salmanmahmoud70@gmail.com>
2023-06-19 02:47:50 +0300

    merge

--- src/app/api/forms/route.js
+++ src/app/api/forms/route.js
@@ -8,31 +8,5 @@ export async function GET(request) {
     results: data.length,
     data,
   }
-
-  console.log(data, 'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
   return new Response(json_response)
 }
-
-// import fs from 'fs'
-// import path from 'path'
-
-// const handler = (req, res) => {
-//   if (req.method === 'POST') {
-//     console.log(req.body)
-//     const feedbackPath = path.join(process.cwd(), 'data', 'feedback.json')
-//     const feedbackOldData = fs.readFileSync(feedbackPath)
-//     const data = JSON.parse(feedbackOldData)
-//     const addNewData = data.push(req.body)
-//     fs.writeFileSync(feedbackPath, JSON.stringify(data))
-
-//     return res
-//       .status(200)
-//       .json({ method: 'Post', message: 'done', data: req.body })
-//   } else if (req.method === 'GET') {
-//     return res.status(200).json({ method: 'GET' })
-//   } else {
-//     return res.status(200).json({ method: 'other' })
-//   }
-// }
-
-// export default handler
